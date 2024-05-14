1c4e5c99c035cd5486440a34fc0c4d1e623cd4df
zhenda <1955093734@qq.com>
2024-05-15 01:19:01 +0800

    update: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,4 @@ sudo chown ubuntu:ubuntu db.sqlite3
 ${env_bin}/python3 manage.py migrate
 ${env_bin}/python3 manage.py collectstatic
 
-# /www/server/pyporject_evn/94d7bd8179533c59aca987c718472328_venv/bin/python3 manage.py makemigrations
-# /www/server/pyporject_evn/94d7bd8179533c59aca987c718472328_venv/bin/python3 manage.py migrate
-# /www/server/pyporject_evn/94d7bd8179533c59aca987c718472328_venv/bin/python3 manage.py collectstatic
-
 # nohup /www/server/pyporject_evn/bee2c6e1ee17c225fec353492bd9078b_venv/bin/python3 manage.py runserver 0.0.0.0:8200 &
