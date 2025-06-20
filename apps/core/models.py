dca917275df7bb781790fe0883ce9d178e646c00
zhenda <msoft77564532@outlook.com>
2025-06-20 15:31:42 +0800

    add(core.models): comments

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+# 常用field备注
+# user = models.ForeignKey(User, verbose_name='用户', on_delete=models.CASCADE)
+# public = models.BooleanField(verbose_name='是否公开', default=False)
+
+
+
 class BaseModel(models.Model):
     """为模型类补充字段"""
     create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
